6bcb5897181b3fa492ae3356729396898f9315b7
jorgeamadosoria <darksoul.uci@gmail.com>
2018-09-23 23:36:13 -0300

    initial commit

---
+++ src/main/java/org/jasr/portfolio/PortfolioApplication.java
@@ -0,0 +1,42 @@
+package org.jasr.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class PortfolioApplication {
+	
+	@Bean
+	  public WebSecurityConfigurerAdapter webSecurityConfig() {
+	      return new WebSecurityConfigurerAdapter() {
+	          @Override
+	          protected void configure(HttpSecurity http) throws Exception {
+	              http.authorizeRequests()
+	                  .antMatchers("/admin/**").authenticated()
+	                  .and()
+	                  .antMatcher("/**").anonymous()
+	                  .and()
+	                  .formLogin();
+	          }
+
+	          @Override
+	          protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+	              BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	              builder.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+	                     .withUser("admin").password(passwordEncoder.encode("admin")).roles("USER")
+	              ;
+	          }
+	      };
+	  }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+}
---
+++ src/main/java/org/jasr/portfolio/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package org.jasr.portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+    @GetMapping("/index")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
