b10ec6426c639a0590289b28b7d0b98902cf9467
jorgeamadosoria <darksoul.uci@gmail.com>
2018-09-25 23:50:25 -0300

    began working on admin controller

--- src/main/java/org/jasr/portfolio/controllers/AdminController.java
+++ src/main/java/org/jasr/portfolio/controllers/AdminController.java
@@ -1,19 +1,42 @@
 package org.jasr.portfolio.controllers;
 
+import org.jasr.portfolio.entities.Type;
+import org.jasr.portfolio.repositories.LinkRepository;
+import org.jasr.portfolio.repositories.ProjectRepository;
+import org.jasr.portfolio.repositories.TagRepository;
+import org.jasr.portfolio.repositories.TypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
+	@Autowired
+	private TypeRepository typeRepository;
+	@Autowired
+	private LinkRepository linkRepository;
+	@Autowired
+	private TagRepository tagRepository;
+	@Autowired
+	private ProjectRepository projectRepository;
 
     @GetMapping("/index")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String index(Model model) {
+        model.addAttribute("types", typeRepository.findAll());
+        model.addAttribute("tags", tagRepository.findAll());
+        model.addAttribute("projects", projectRepository.findAll());
         return "index";
     }
 
+    @PostMapping("/type")
+    public String type(@ModelAttribute  Type type) {
+    	
+    	typeRepository.saveAndFlush(type);
+        return "redirect:index";
+    }
 }
