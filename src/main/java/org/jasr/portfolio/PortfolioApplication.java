c4adf6a22cb1064146116237b31b0b11c5ef6559
jorgeamadosoria <darksoul.uci@gmail.com>
2018-10-18 01:05:21 -0300

    removed references to Liquibase as not a suitable tool

--- src/main/java/org/jasr/portfolio/PortfolioApplication.java
+++ src/main/java/org/jasr/portfolio/PortfolioApplication.java
@@ -1,5 +1,8 @@
 package org.jasr.portfolio;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,38 +16,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication implements WebMvcConfigurer {
-	
+
+	@Autowired
+	private DataSource dataSource;
+
 	@Bean
-	  public WebSecurityConfigurerAdapter webSecurityConfig() {
-	      return new WebSecurityConfigurerAdapter() {
-	          @Override
-	          protected void configure(HttpSecurity http) throws Exception {
-	              http.authorizeRequests()
-	                  .antMatchers("/admin/**").authenticated()
-	                  .and()
-	                  .antMatcher("/**").anonymous()
-	                  .and()
-	                  .logout().permitAll()
-	                  .and()
-	                  .formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/admin/index").permitAll();
-	          }
+	public WebSecurityConfigurerAdapter webSecurityConfig() {
+		return new WebSecurityConfigurerAdapter() {
+			@Override
+			protected void configure(HttpSecurity http) throws Exception {
+				http.authorizeRequests().antMatchers("/admin/**").authenticated().and().antMatcher("/**").anonymous().and().logout().permitAll().and().formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/admin/index").permitAll();
+			}
 
-	          @Override
-	          protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-	              BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	              builder.inMemoryAuthentication().passwordEncoder(passwordEncoder)
-	                     .withUser("admin").password(passwordEncoder.encode("admin")).roles("USER")
-	              ;
-	          }
-	      };
-	  }
-	
+			@Override
+			protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+				BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+				builder.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder);
+			}
+		};
+	}
+
 	@Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
---
+++ src/main/java/org/jasr/portfolio/entities/User.java
@@ -0,0 +1,44 @@
+package org.jasr.portfolio.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	private String password;
+	private boolean enabled;
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
